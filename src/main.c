d1bbdbbee3eda45c0ab8a25645427a1407afe86b
fattah <herppratama@gmail.com>
2018-11-15 14:51:25 +0700

    main udah, komentar to go

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include "lib/pda.h"
+#include <stdio.h>
+
+int main(){
+	Stack S;
+	char input[1000];
+
+	// fgets(input, 1000, stdin);
+	scanf("%s", &input);
+
+	if(PDA(input)){
+		float result = StringHandler(input);
+		printf("%.2f\n", result);
+	} else {
+		printf("Syntax Error\n");
+	}
+}
