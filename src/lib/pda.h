135a4295514713b7e917564c1e9a2caf517dd00a
fattah <herppratama@gmail.com>
2018-11-15 09:44:17 +0700

    yah begitulah

---
+++ src/lib/pda.h
@@ -0,0 +1,39 @@
+#ifndef
+#define pdacalculator_H
+
+#include "boolean.h"
+#include "string.h"
+#include "stackt.h"
+#include <stdio.h>
+
+float StringToFloat(char *s);
+/* Mengubah string dari sebuah bilangan ke bentuk float
+I.S : s terdefinisi sebagai sebuah string berbentuk bilangan dan tidak kosong,
+minimal adalah 2 karakter untuk bilangan negatif dan 1 karakter untuk bil. positif
+F.S : mengembalikan hasil konversi dari string s
+*/
+void Transition(int *State, char Symbol, Stack *S,boolean *stuck);
+/* Prosedur transisi PDA
+State terdefinisi 0,1,2,3. Symbol merupakan karakter yang sedang dibaca. S merupakan Stack yang digunakan. stuck bernilai true jika Symbol tidak valid
+*/
+
+boolean PDA(char *str);
+/* Validasi apakah string merupakan ekspresi matematika yang valid?
+I.S : str terdefinisi sebagai sebuah string
+F.S : jika str merupakan ekspresi matematika, output true. Jika tidak, output false
+*/
+
+float NgitungString(char *s);
+/* Menghitung operasi matematika dari string yang diinputkan
+Prekondisi  : Sintaks string harus benar
+F.S         : Mengeluarkan hasil dari operasi matematika string tersebut
+Operasi yang ada (+,-,*,/,()) 
+*/
+
+float NgitungKurung(char *s);
+/* Algoritma khusus menghitung operasi dalam kurung
+Prekondisi  : Sintaks merupakan operasi dalam kurung
+F.S         : Menghasilkan hasil operasi dalam kurung
+*/
+
+#endif
