334c6ead4db6f1e845d6a387b6d0ad3d0654115f
zaldoiii <43696167+zaldoiii@users.noreply.github.com>
2018-11-20 18:33:55 +0700

    Add files via upload
    Biar ga capek compile and run

--- src/main.c
+++ src/main.c
@@ -1,21 +1,35 @@
-#include "lib/pda.h"
 #include <stdio.h>
+#include <string.h>
+#include "lib/pda.c"
+#include "lib/stack.c"
 
-int main(){
-	Stack S;
+int main()
+{
+	// Stack S;
 	char input[1000];
 
 	// fgets(input, 1000, stdin);
-	scanf("%s", input);
-
-	if(PDA(input)){
-		float result = StringHandler(input);
-		if(result != result){
-			printf("Math Error\n");
-		} else {
-			printf("%.2f\n", result);
+	printf("Ketik EXIT untuk keluar program\n\n"); 
+	do
+	{
+		scanf("%s",input);
+		if (PDA(input))
+		{
+			float result = StringHandler(input);
+			if(result != result)
+			{
+				printf("Math Error\n");
+			} 
+			else 
+			{
+				printf("-> ");
+				printf("%.2f\n\n", StringHandler(input));
+			}
+		} 
+		else if (strcmp(input,"EXIT") != 0)
+		{
+			printf("Syntax Error\n\n");
 		}
-	} else {
-		printf("Syntax Error\n");
 	}
+	while (strcmp(input,"EXIT") != 0);
 }
